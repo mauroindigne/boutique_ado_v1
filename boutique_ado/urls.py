5644b8fe6ce60bb1a21fd98e428f5833bc78eefc
mauroindigne <mauroindigne@gmail.com>
2020-08-03 17:04:57 +0000

    fixed

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,4 +1,4 @@
-"""boutique_ado_v1 URL Configuration
+"""boutique_ado URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
